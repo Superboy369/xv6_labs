61adfeb09842c6c2851bd20c8ed9b3b83cd563b3
vboxuser <vboxuser@ubantu01.myguest.virtualbox.org>
2024-02-06 11:29:54 +0800

    'Course thread switching' added the utility 'spin' to learn the process of the switching threads.

---
+++ user/spin.c
@@ -0,0 +1,24 @@
+// my spin.c
+
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc,char * argv[]){
+    int pid;
+    char c;
+
+    pid = fork();
+    if(pid == 0){
+        c = '/';
+    }else{
+        printf("parent pid id %d\n, child is %d\n",getpid(),pid);
+        c = '\\';
+    }
+    for(int i = 0;;i ++ ){
+        if((i % 1000000) == 0){
+            write(2,&c,1);
+        }
+    }
+
+    exit(0);
+}
